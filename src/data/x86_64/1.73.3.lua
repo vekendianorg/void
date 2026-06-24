e95c84e02d1c7cfe145c23eb6943a652a707046e
AMR <abdullahmahirofficial@gmail.com>
2026-06-25 00:27:13 +0600

    Update 1.73.3.lua

--- src/data/x86_64/1.73.3.lua
+++ src/data/x86_64/1.73.3.lua
@@ -6,39 +6,33 @@
 return {
     aobs = {
         fakeVip = {
-            {scan = "h 93 D6 01 F9 68 B2 40 39 1F 01 00 71", offset = 4, patch = "h 28 00 80 52", unpatch = "h 68 B2 40 39"},
+            {scan = "h 12 C0 00 55 48 89 E5 41 56 53", offset = 3, patch = "h B0 01 C3", unpatch = "h 55 48 89"},
         },
 
         fakeUnlock = {
-            {scan = "h 36 C5 40 F9", pattern = { { offset = 0x164, valid = {"h E0 03 1F 2A"} }, { offset = 0x16C, valid = {"h 20 00 80 52"} } }, offset = 0x164, patch = "h 20 00 80 52", unpatch = "h E0 03 1F 2A"},
+            {scan = "h 92 C0 00 55 48 89 E5 41 57 41 56 41 55 41 54", offset = 3, patch = "h B8 01 00 00 00 C3", unpatch = "h 55 48 89 E5 41 57"},
         },
 
         autoDetach = {
-            {scan = "h 08 20 20 1E 85 00 00 54 E0 03 13 AA E1 03 14 AA", offset = 4, patch = "h 1F 20 03 D5", unpatch = "h 85 00 00 54"},
+            {scan = "h C0 41 0F 2E 46 30 76 0B 48 89 DF", offset = 4, patch = "h 46 30 90 90", unpatch = "h 46 30 76 0B"},
         },
 
-        autoWin = {
-            {scan = "h E8 5F 5D A9 16 61 40 B9", offset = 4, patch = "h 55 00 80 52", unpatch = "h 16 61 40 B9"},
-            {scan = "h E0 5F 40 F9 09 4D 40 BD", offset = 4, patch = "h 0A 90 32 1E", unpatch = "h 09 4D 40 BD"},
+        autoWin = {                                                                                                                                                        -- setFirstPos, instantDie
+            {scan = "h 60 F3 0F 10 40 4C F3 0F 11 44 24 0C 48", offset = 1, patch = "h C7 44 24 0C 00 00 A0 C0 90 90 90", unpatch = "h F3 0F 10 40 4C F3 0F 11 44 24 0C"}, -- setFirstPos
+            {scan = "h 8B 40 60 89 44 24 04", offset = 0, patch = "h 6A 02 8F 44 24 04 90", unpatch = "h 8B 40 60 89 44 24 04"},                                           -- instantDie
         },
 
-        autoDie = {
-            {scan = "h 60 56 08 BD 60 56 48 BD 08 20 20 1E", offset = 12, patch = "h 00 00 80 52", unpatch = "h 45 00 00 54"},
-            {scan = "h 60 56 08 BD 60 56 48 BD 08 20 20 1E", offset = 16, patch = "h 60 56 08 B9", unpatch = "h 7F 56 08 B9"},
+        autoDie = {                                                                                               -- freeze, noFuel
+            {scan = "h 49 89 47 50 48 8B 43 30", offset = 0, patch = "h 90 90 90 90", unpatch = "h 49 89 47 50"}, -- freeze
+            {scan = "h C9 0F 2E C8 76 0A C7 83", offset = 4, patch = "h 90 90", unpatch = "h 76 0A"},             -- noFuel
         },
 
-        forceBoss = {
-            {scan = "h 00 CD 41 BD FD 7B C1 A8 C0 03 5F D6", offset = 0, patch = "h 00 C1 5F BC", unpatch = "h 00 CD 41 BD"},
-            {scan = "h 00 29 44 BD FD 7B C1 A8 C0 03 5F D6", offset = 0, patch = "h 00 C1 5F BC", unpatch = "h 00 29 44 BD"},
-        },
-
-        fuel = {
-            {scan = "h 61 56 48 BD 00 C0 22 1E 21 C0 22 1E 00 84 48 1F 00 40 62 1E 60 56 08 BD", offset = 4},
+        forceBoss = {                                                                                                -- Trophy, Legendary
+            {scan = "h F8 F3 0F 10 80 28 04 00 00", offset = 5, patch = "h FC FF FF FF", unpatch = "h 28 04 00 00"}, -- Trophy
+            {scan = "h F8 F3 0F 10 80 CC 01 00 00", offset = 5, patch = "h FC FF FF FF", unpatch = "h CC 01 00 00"}, -- Legendary
         },
     },
     offsets = {
-        raceInfo     = 0x2066508, -- x86_64-specific
-        vnpStats     = 0x2060CA0,
-        customTracks = 0x1FE27F0,
+        raceInfo = 0x2066508,     -- race info like distance, cd
     },
 }
