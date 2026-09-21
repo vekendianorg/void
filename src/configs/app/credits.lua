5859a4f4e77b79b342fc1bb3b0eb10f8f6ef0b26
AMR <amrgg.dev@gmail.com>
2026-09-21 22:24:49 +0600

    Update credits.lua

--- src/configs/app/credits.lua
+++ src/configs/app/credits.lua
@@ -36,6 +36,9 @@ return {
 - Українська: AMR (Discord: amrgg)
 - Türkçe: AMR (Discord: amrgg)
 - Português (Brasil): AMR (Discord: amrgg)
+- हिन्दी (Hindi): AMR (Discord: amrgg)
+- Italiano (Italian): AMR (Discord: amrgg)
+- فارسی (Persian): AMR (Discord: amrgg)
 - 简体中文 (Simplified Chinese): Lan9118 (Discord: lan9118)
 ]],
 
@@ -47,6 +50,10 @@ return {
 - Erik (Discord: eomthix)
 - Sr Romero
 - Profinoobru
+- No Fear
+- Rei
+- donuts
+- dxzzy
 ]],
 
 -- ── Special thanks ────────────────────────────────────────────────────────
